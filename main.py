1736c39f7cdbc98f42c2773f578a720ad71b7eb4
asr-johnnette <asr.johnnette@gmail.com>
2023-12-07 17:42:29 +0530

    adding gsteamer link to the file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import subprocess
 
 def connect_to_camera(ip_address):
+    # gst-launch-1.0 rtspsrc location=rtsp://192.168.1.39:8554/fpv_stream ! rtph264depay ! h264parse ! avdec_h264 ! identity drop-allocation=true ! autovideosink sync=false
     pipeline_command = f"rtspsrc location=rtsp://{ip_address}:8554/fpv_stream ! rtph264depay ! h264parse ! avdec_h264 ! identity drop-allocation=true ! autovideosink sync=false"
     print(pipeline_command)
 
